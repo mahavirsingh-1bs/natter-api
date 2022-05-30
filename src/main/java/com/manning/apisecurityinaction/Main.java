ac414272714572d646af4a09aa7153d396bdc082
Mahavir Singh <msingh@Macs-MacBook-Pro.local>
2022-05-30 16:51:01 +0530

    Token-based authentication: bearer token

---
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -0,0 +1,153 @@
+package com.manning.apisecurityinaction;
+
+import static spark.Spark.after;
+import static spark.Spark.afterAfter;
+import static spark.Spark.before;
+import static spark.Spark.delete;
+import static spark.Spark.exception;
+import static spark.Spark.get;
+import static spark.Spark.halt;
+import static spark.Spark.internalServerError;
+import static spark.Spark.notFound;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import static spark.Spark.secure;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Set;
+
+import org.dalesbred.Database;
+import org.dalesbred.result.EmptyResultException;
+import org.h2.jdbcx.JdbcConnectionPool;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.common.util.concurrent.RateLimiter;
+import com.manning.apisecurityinaction.controller.AuditController;
+import com.manning.apisecurityinaction.controller.ModeratorController;
+import com.manning.apisecurityinaction.controller.SpaceController;
+import com.manning.apisecurityinaction.controller.TokenController;
+import com.manning.apisecurityinaction.controller.UserController;
+import com.manning.apisecurityinaction.token.CookieTokenStore;
+import com.manning.apisecurityinaction.token.DatabaseTokenStore;
+import com.manning.apisecurityinaction.token.TokenStore;
+
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
+public class Main {
+
+	public static void main(String... args) throws Exception {
+		secure("localhost.p12", "changeit", null, null);
+		port(args.length > 0 ? Integer.parseInt(args[0]) 
+				: spark.Service.SPARK_DEFAULT_PORT);
+		Spark.staticFiles.location("/public");
+		
+		var rateLimiter = RateLimiter.create(2.0d);
+		before((request, response) -> {
+			if (!rateLimiter.tryAcquire()) {
+				response.header("Retry-After", "2");
+				halt(429);
+			}
+		});
+		before(new CorsFilter(Set.of("https://localhost:9999")));
+		
+		before(((request, response) -> {
+            if (request.requestMethod().equals("POST") &&
+            !"application/json".equals(request.contentType())) {
+                halt(415, new JSONObject().put(
+                    "error", "Only application/json supported"
+                ).toString());
+            }
+        }));
+		
+		var datasource = JdbcConnectionPool.create("jdbc:h2:mem:natter", "natter", "password");
+		var database = Database.forDataSource(datasource);
+		createTables(database);
+		
+		datasource = JdbcConnectionPool.create("jdbc:h2:mem:natter", "natter_api_user", "password");
+		database = Database.forDataSource(datasource);
+		
+		var spaceController = new SpaceController(database);
+		var userController = new UserController(database);
+		
+		TokenStore tokenStore = new DatabaseTokenStore(database);
+		var tokenController = new TokenController(tokenStore);
+		
+		before(userController::authenticate);
+		before(tokenController::validateToken);
+		
+		var auditController = new AuditController(database);
+		before(auditController::auditRequestStart);
+		
+		before("/sessions", userController::requireAuthentication);
+		post("/sessions", tokenController::login);
+		delete("/sessions", tokenController::logout);
+		
+		before("/spaces", userController::requireAuthentication);
+		
+		before("/spaces", userController::requireAuthentication); 
+		post("/spaces", spaceController::createSpace);
+		
+		before("/spaces/:spaceId/messages", userController.requirePermission("POST", "w"));
+		post("/spaces/:spaceId/messages", spaceController::postMessage);
+			 
+		before("/spaces/:spaceId/messages/*", userController.requirePermission("GET", "r"));
+		get("/spaces/:spaceId/messages/:msgId", spaceController::readMessage);
+			 
+		before("/spaces/:spaceId/messages", userController.requirePermission("GET", "r"));
+		get("/spaces/:spaceId/messages", spaceController::findMessages);
+		
+		var moderatorController = new ModeratorController(database);
+
+		before("/spaces/:spaceId/messages/*", userController.requirePermission("DELETE", "d"));
+		delete("/spaces/:spaceId/messages/:msgId", moderatorController::deletePost);
+		
+		before("/spaces/:spaceId/members", userController.requirePermission("POST", "rwd"));
+		post("/spaces/:spaceId/members", spaceController::addMember);
+		
+		post("/users", userController::registerUser);
+		get("/logs", auditController::readAuditLog);
+		
+		after((request, response) -> {
+			response.type("application/json");
+		});
+		
+		exception(IllegalArgumentException.class, Main::badRequest);
+		exception(JSONException.class, Main::badRequest);
+		exception(EmptyResultException.class, 
+				(e, request, response) -> response.status(404));
+		
+		internalServerError(new JSONObject()
+			.put("error", "internal server error").toString());
+		notFound(new JSONObject()
+			.put("error", "not found").toString());
+		
+		after(auditController::auditRequestEnd);
+		afterAfter((request, response) -> {
+			response.type("application/json;charset=utf-8");
+			response.header("X-Content-Type-Options", "nosniff");
+			response.header("X-Frame-Options", "DENY");
+			response.header("X-XSS-Protection", "0");
+			response.header("Cache-Control", "no-store");
+			response.header("Content-Security-Policy",
+                    "default-src 'none'; frame-ancestors 'none'; sandbox");
+			response.header("Strict-Transport-Security", "max-age=3");
+			response.header("Server", "");
+		});
+	}
+
+	private static void badRequest(Exception ex, Request request, Response response) {
+		response.status(400);
+		response.body(new JSONObject()
+				.put("error", ex.getMessage()).toString());
+	}
+	
+	private static void createTables(Database database) throws Exception {
+		var path = Paths.get(Main.class.getResource("/schema.sql").toURI());
+		database.update(Files.readString(path));
+	}
+
+}
